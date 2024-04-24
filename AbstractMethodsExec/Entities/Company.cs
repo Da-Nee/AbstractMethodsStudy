e213ea1c570e206e0fb71779bcd4d219ae53e59f
Felipe Yohan Sapio <yohansapio@rocketmail.com>
2024-04-24 00:58:32 -0300

    Commit

---
+++ AbstractMethodsExec/Entities/Company.cs
@@ -0,0 +1,24 @@
+﻿namespace AbstractMethosExec.Entities
+{
+    class Company : TaxPayer
+    {
+        public int Employees { get; set; }
+
+        public Company(string name, double annualIncome, int employees) : base(name, annualIncome)
+        {
+            Employees = employees;
+        }
+
+        public override double Tax()
+        {
+            if (Employees > 0)
+            {
+                return AnnualIncome * 0.14;
+            }
+            else
+            {
+                return AnnualIncome * 0.16;
+            }
+        }
+    }
+}
---
+++ AbstractMethodsExec/Entities/Individual.cs
@@ -0,0 +1,23 @@
+﻿namespace AbstractMethosExec.Entities
+{
+    class Individual : TaxPayer
+    {
+        public double HealthExpenditures { get; set; }
+        public Individual(string name, double annualIncome, double healthExpenditures) : base(name, annualIncome)
+        {
+            HealthExpenditures = healthExpenditures;
+        }
+        public override double Tax()
+        {
+            if (AnnualIncome < 20000.00)
+            {
+                return (AnnualIncome * 0.15) - (HealthExpenditures * 0.50);
+            }
+            else
+            {
+                return AnnualIncome * 0.25 - (HealthExpenditures * 0.50);
+            }
+
+        }
+    }
+}
